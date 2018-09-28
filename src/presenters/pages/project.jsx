664dc6e39eb7638d88b1323a18e58e4c3d4c23bb
Glitch (delirious-bite) <none>
2018-09-28 14:56:12 +0000

    🌁🏬 Checkpoint ./src/presenters/pages/project.jsx:9/75 ./styles/profile.styl:9/2

--- src/presenters/pages/project.jsx
+++ src/presenters/pages/project.jsx
@@ -129,8 +129,6 @@ const InterestingPackageJsonThings = ({data}) => {
   };
   
   const currentProjectLicenseLogo = licenseLogos[data["license"]];
-  console.log(currentProjectLicenseLogo);
-  
 
 
   return (
